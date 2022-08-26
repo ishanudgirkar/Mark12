b4111df4058e72d2acb85953565067339d747fd0
Ishan Udgirkar <udgirkarishan@gmail.com>
2022-08-27 00:16:14 +0530

    Still long to go

---
+++ istriangle.js
@@ -0,0 +1,20 @@
+const inputs = document.querySelectorAll('.angle-input');
+const isTriangleBtn = document.querySelector('#is-triangle-btn');
+const outputEl = document.querySelector('#output');
+
+function calculateSumOfAngles(angle1 , angle2 ,angle3){
+
+    const sumOfAngles = angle1 + angle2 + angle3;
+    // console.log(sumOfAngles);
+    return sumOfAngles;
+
+}
+
+function isTriangle(){
+
+    const sumOfAngles = calculateSumOfAngles(Number(inputs[0].value ),Number(inputs[0].value ),Number(inputs[0].value ));
+
+    
+}
+
+isTriangleBtn.addEventListener("click",isTriangle)
