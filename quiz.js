463e9f1173e634723bdc870f0de505b385b8e806
Ishan Udgirkar <udgirkarishan@gmail.com>
2022-09-13 23:22:29 +0530

    Done !

--- quiz.js
+++ quiz.js
@@ -8,14 +8,18 @@ function calculateScore(params) {
     let score = 0;
     let index = 0;
     const formResults = new FormData(quizForm);
-    for(let value of formResults.values()){
-        if(value === correctAnswers[index]){
-            score += 1 ;
+    if (formResults.entries().next().done) {
+      outputEl.innerText = "please select all the choices first !";
+    } else {
+      for (let value of formResults.values()) {
+        if (value === correctAnswers[index]) {
+          score += 1;
         }
         index += 1;
+      }
+      // console.log(score);
+      outputEl.innerText = "Your score is " + score;
     }
-    // console.log(score);
-    outputEl.innerText = "Your score is "+ score;
-}
+  }
 
 submitAnswerBtn.addEventListener('click',calculateScore);
