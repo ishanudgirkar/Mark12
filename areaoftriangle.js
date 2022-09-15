54610c9b757999ce0c377518d58f206c053339a0
Ishan Udgirkar <udgirkarishan@gmail.com>
2022-09-15 22:50:12 +0530

    Handled the negative input case

--- areaoftriangle.js
+++ areaoftriangle.js
@@ -8,9 +8,12 @@ function calculateArea(){
     const base = Number(baseOfTriangle.value);
     const height = Number(heightOfTriangle.value);
     if(base && height){
+        if(base < 0 || height < 0){
+            outputEl.innerText = "How can be a base OR height of a triangle a negative entity ? 😒"
+        }else{
     const Area = Number(0.5) * base * height;
     outputEl.innerText = "Area Of Triangle is " + Area + " units";
-    }else{
+    }}else{
         outputEl.innerText = "Please Enter base & height ";
     }
 
